6698c8610df5a5d613a645d3946885e3b5b9fff1
ToberlerOhn <tobyparadise16@gmail.com>
2026-06-22 22:15:03 -0400

    Vibecoded hover support

---
+++ hades-language/extension.js
@@ -0,0 +1,41 @@
+// following code is purely vibe coded
+// I don't have a clue how to make this especially in js
+// documentation is human made
+
+const vscode = require('vscode');
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+    // Basic documentation database for Hades
+    const hadesDocs = {
+        'print': '### `print\nPrints to terminal.',
+    };
+
+    // Register the hover provider for the 'hades' language
+    const hoverProvider = vscode.languages.registerHoverProvider('hades', {
+        provideHover(document, position, token) {
+            // Get the word currently under the mouse cursor
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+
+            // Check if we have documentation for this specific word
+            if (hadesDocs[word]) {
+                return new vscode.Hover(new vscode.MarkdownString(hadesDocs[word]));
+            }
+
+            // Return undefined if we don't have a hover card for this word
+            return undefined;
+        }
+    });
+
+    context.subscriptions.push(hoverProvider);
+}
+
+function deactivate() {}
+
+module.exports = {
+    activate,
+    deactivate
+};
